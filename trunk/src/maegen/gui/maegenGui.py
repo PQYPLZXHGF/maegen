3c534bbdc9ba90d60df182b8128d49dfff47991d
tbressure@gmail.com <tbressure@gmail.com@c8d97e15-afbc-8726-11a9-535a644ef7df>
2011-11-05 21:56:54 +0000

    the gui call correct method on facade to set both father and mother

--- trunk/src/maegen/gui/maegenGui.py
+++ trunk/src/maegen/gui/maegenGui.py
@@ -692,28 +692,25 @@ class IndividualView(MaegenStackableWindow):
             
     def on_save_clicked_event(self, widget, data):
         
-               
-        if self.edit_father and self.edit_mother:
-            if self.father_enabled.get_active() and self.mother_enabled.get_active():
-              pass         
+        if self.father_enabled.get_active() and self.mother_enabled.get_active():               
+            if self.edit_father and self.edit_mother:
+                self.zcore.set_parents(self.individual, self.edit_father, self.edit_mother)
+            elif self.edit_father:
+                self.zcore.set_father(self.individual,self.edit_father)
+            elif self.edit_mother:            
+                self.zcore.set_mother(self.individual,self.edit_mother)
         else:    
-            if self.edit_father:
-                if self.father_enabled.get_active():
-                    self.zcore.set_father(self.individual,self.edit_father)
-                else:
-                    self.zcore.remove_father(self.individual)                        
+            if self.father_enabled.get_active():
+                if self.edit_father:                
+                    self.zcore.set_father(self.individual,self.edit_father)                    
             elif self.individual.father:
-                if not self.father_enabled.get_active() :
-                    self.zcore.remove_father(self.individual)
+                self.zcore.remove_father(self.individual)
             
-            if self.edit_mother:            
-                if self.mother_enabled.get_active():
+            if self.mother_enabled.get_active():
+                if self.edit_mother:                            
                     self.zcore.set_mother(self.individual,self.edit_mother)
-                else:
-                    self.zcore.remove_mother(self.individual)
-            elif self.individual.mother: 
-                if not self.mother_enabled.get_active():
-                    self.zcore.remove_mother(self.individual)
+            elif self.individual.mother:                 
+                self.zcore.remove_mother(self.individual)
             
         self.individual.name = self.edit_name.get_text()
         self.individual.firstname = self.edit_firstname.get_text()
