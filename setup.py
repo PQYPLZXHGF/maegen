7c9d0a428c561cfed267e253f01658c6ddc781d2
tbressure <tbressure@gmail.com>
2011-11-06 20:08:11 +0000

    next dev cycle

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ ez_setup.use_setuptools()
 
 from setuptools import setup, find_packages
 setup(name='maegen',
-      version='0.1.0',
+      version='0.2.0',
       package_dir = {'': 'src'},
       packages=find_packages(where='src'),               
       scripts=['scripts/maegen'],
